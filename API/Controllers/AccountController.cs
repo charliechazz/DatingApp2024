400acdb26bbf05a6421800ecfe8e8070f66894f0
Charlie <charlychaky567@hotmail.com>
2024-09-21 16:48:50 -0600

    Parcial01 - se agregó RegisterDTO

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,14 +10,14 @@ namespace API.Controllers;
 public class AccountController(DataContext context) : BaseApiController
 {
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> RegisterAsync(string username, string password)
+        public async Task<ActionResult<AppUser>> RegisterAsync(RegisterDTO register)
         {
             using var hmac = new HMACSHA512();
             
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = register.Username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(register.Password)),
                 PasswordSalt = hmac.Key
             };
 
