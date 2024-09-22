7a348acc7c11c1c3d1bb8770641eb95f46b8764b
Charlie <charlychaky567@hotmail.com>
2024-09-22 00:07:45 -0600

    Parcial01 - Add Token service

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,35 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using API.Entities;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services;
+public class TokenService(IConfiguration config) : ITokenService
+{
+    public string CreateToken(AppUser user)
+    {
+        var tokenkey = config["TokeKey"] ?? throw new Exception("Tokenkey not found");
+        if(tokenkey.Length < 64) throw new Exception("Tokenkey too short");
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenkey));
+
+        var claims = new List<Claim>
+        {
+            new (ClaimTypes.NameIdentifier, user.UserName),
+        };
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = creds
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
