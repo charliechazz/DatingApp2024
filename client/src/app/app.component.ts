2583bd357d07801fdb88fd42502a01d51eab797b
Charlie <charlychaky567@hotmail.com>
2024-09-29 00:27:34 -0600

    Parcial01 - Home Component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { Observable } from 'rxjs';
 import { AccountService } from './_services/account.service';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor, NavComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
