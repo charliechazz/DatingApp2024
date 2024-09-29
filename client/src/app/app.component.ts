57bc1475bf75eeff9c1450780a5ab9e37a7bbe47
Charlie <charlychaky567@hotmail.com>
2024-09-29 00:11:44 -0600

    Parcial01 - Persist the login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
+import { Observable } from 'rxjs';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -12,17 +14,29 @@ import { NavComponent } from "./nav/nav.component";
   styleUrl: './app.component.css'
 })
 export class AppComponent  implements OnInit{
-
   http = inject(HttpClient)
+  private accountService = inject(AccountService);
   title = 'Date me';
   users: any;
 
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrenUser();
+  }
+
+  setCurrenUser(){
+    const userString = localStorage.getItem("user");
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  getUsers() {
     this.http.get("https://localhost:5001/api/users").subscribe({
-      next: (response) => {this.users = response},
-      error: (error) => {console.log(error)},
-      complete: () => {console.log("Request completed!")}
-    } 
-    )
+      next: (response) => { this.users = response; },
+      error: (error) => { console.log(error); },
+      complete: () => { console.log("Request completed!"); }
+    }
+    );
   }
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -11,22 +11,20 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-  private accountService = inject(AccountService);
-  loggedIn = false;
+  accountService = inject(AccountService);
   model: any ={};
 
   login(): void {
     this.accountService.login(this.model).subscribe({
       next: (response) => {
         console.log(response);
-        this.loggedIn = true;
       },
       error: error => console.log(error)
     })
   }
 
   logout(): void{
-    this.loggedIn = false;
+    this.accountService.logout();
   }
 
 }
