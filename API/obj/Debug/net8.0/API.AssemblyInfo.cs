43e67d77fed18290709a94709e15e6407ab95879
Charlie <charlychaky567@hotmail.com>
2024-11-13 20:56:00 -0600

    Parcial03 - actualización

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+717acb1f5223288170bf4b7aa131e1199c58d3d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7f0192697717c8faf33ead1bf2b8ec338b236be")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
