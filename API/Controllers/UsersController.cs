f69815bc33f6a2de7a7aca73b9cefbeab3a46160
Charlie <charlychaky567@hotmail.com>
2025-02-09 10:37:23 -0600

    Parcial04 - Add PhotoService

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,7 +1,7 @@
 namespace API.Controllers;
 
-using System.Security.Claims;
 using API.Data;
+using API.DataEntities;
 using API.DTOs;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -11,11 +11,13 @@ using Microsoft.AspNetCore.Mvc;
 public class UsersController : BaseApiController
 {
     private readonly IUserRepository _repository;
+    private readonly IPhotoService _photoService;
     private readonly IMapper _mapper;
 
-    public UsersController(IUserRepository repository, IMapper mapper)
+    public UsersController(IUserRepository repository, IPhotoService photoService, IMapper mapper)
     {
         _repository = repository;
+        _photoService = photoService;
         _mapper = mapper;
     }
 
@@ -26,7 +28,7 @@ public class UsersController : BaseApiController
         return Ok(members);
     }
 
-    [HttpGet("{username}")] // api/users/Calamardo
+    [HttpGet("{username}", Name = "GetByUsername")] // api/users/Calamardo
     public async Task<ActionResult<MemberResponse>> GetByUsernameAsync(string username)
     {
         var member = await _repository.GetMemberAsync(username);
@@ -42,14 +44,7 @@ public class UsersController : BaseApiController
     [HttpPut]
     public async Task<ActionResult> UpdateUser(MemberUpdateRequest request)
     {
-        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-
-        if (username == null)
-        {
-            return BadRequest("No username found in token");
-        }
-
-        var user = await _repository.GetByUsernameAsync(username);
+        var user = await _repository.GetByUsernameAsync(User.GetUserName());
 
         if (user == null)
         {
@@ -65,5 +60,39 @@ public class UsersController : BaseApiController
         }
 
         return BadRequest("Update user failed!");
+    }
+
+    [HttpPost("photo")]
+    public async Task<ActionResult<PhotoResponse>> AddPhoto(IFormFile file)
+    {
+        var user = await _repository.GetByUsernameAsync(User.GetUserName());
+
+        if (user == null)
+        {
+            return BadRequest("Cannot update user");
+        }
+
+        var result = await _photoService.AddPhotoAsync(file);
+
+        if (result.Error != null)
+        {
+            return BadRequest(result.Error.Message);
+        }
+
+        var photo = new Photo
+        {
+            Url = result.SecureUrl.AbsoluteUri,
+            PublicId = result.PublicId
+        };
+
+        user.Photos.Add(photo);
+
+        if (await _repository.SaveAllAsync())
+        {
+            return CreatedAtAction("GetByUsername",
+                new { username = user.UserName }, _mapper.Map<PhotoResponse>(photo));
+        }
+
+        return BadRequest("Problem adding the photo");
     }
 }
---
+++ API/Services/PhotoService.cs
@@ -0,0 +1,38 @@
+using API.Helpers;
+using API.Services;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.Extensions.Options;
+#pragma warning disable CA1050 // Declarar tipos en espacios de nombres
+public class PhotoService : IPhotoService
+#pragma warning restore CA1050 // Declarar tipos en espacios de nombres
+{
+    private readonly Cloudinary cloudinary;
+    public PhotoService(IOptions<CloudinarySettings> config)
+    {
+        var acc = new Account(config.Value.CloudName, config.Value.ApiKey, config.Value.ApiSecret);
+        cloudinary = new Cloudinary(acc);
+    }
+    public async Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
+    {
+        var uploadResult = new ImageUploadResult();
+        if (file.Length > 0)
+        {
+            using var stream = file.OpenReadStream();
+            var uploadParams = new ImageUploadParams
+            {
+                File = new FileDescription(file.FileName, stream),
+                Transformation = new Transformation()
+                    .Height(500).Width(500).Crop("fill").Gravity("face"),
+                Folder = "spa2025"
+            };
+            uploadResult = await cloudinary.UploadAsync(uploadParams);
+        }
+        return uploadResult;
+    }
+    public async Task<DeletionResult> DeletePhotoAsync(string publicId)
+    {
+        var deleteParams = new DeletionParams(publicId);
+        return await cloudinary.DestroyAsync(deleteParams);
+    }
+}
