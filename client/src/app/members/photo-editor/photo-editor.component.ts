3a4a90d08fe942e21ed872d9fcce258e1dfe12b0
Charlie <charlychaky567@hotmail.com>
2025-02-22 12:03:02 -0600

    Parcial04 - Update client side user registration

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -15,7 +15,6 @@ import { Photo } from '../../_models/photo';
   styleUrl: './photo-editor.component.css'
 })
 export class PhotoEditorComponent implements OnInit {
-  
   private accountService = inject(AccountService);
   private memberService = inject(MembersService);
 
@@ -82,6 +81,19 @@ export class PhotoEditorComponent implements OnInit {
       const updatedMember = { ...this.member() };
       updatedMember.photos.push(photo);
       this.memberChange.emit(updatedMember);
+      if (photo.isMain) {
+        const user = this.accountService.currentUser();
+        if (user) {
+          user.photoUrl = photo.url;
+          this.accountService.setCurrentUser(user);
+        }
+        updatedMember.photoUrl = photo.url;
+        updatedMember.photos.forEach(p => {
+          if (p.isMain) p.isMain = false;
+          if (p.id == photo.id) p.isMain = true;
+        });
+        this.memberChange.emit(updatedMember);
+      }
     }
   }
 }
