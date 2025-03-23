42c99b574da5beeba1f15a0343cab038a19b5a06
Charlie <charlychaky567@hotmail.com>
2025-03-23 10:23:01 -0600

    Parcial05 - Add filter buttons to SPA

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -2,18 +2,22 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { MemberCardComponent } from '../member-card/member-card.component';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { AccountService } from '../../_services/account.service';
+import { UserParams } from '../../_models/userParams';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [MemberCardComponent, PaginationModule],
+  imports: [MemberCardComponent, PaginationModule, FormsModule],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
 export class MemberListComponent implements OnInit {
+  private accountService = inject(AccountService);
   membersService = inject(MembersService);
-  pageNumber = 1;
-  pageSize = 5;
+  userParams = new UserParams(this.accountService.currentUser());
+  genderList = [{value: "female", display: "Females"}, {value: "male", display: "Males"}]
   
   ngOnInit(): void {
     if (!this.membersService.paginatedResult()) {
@@ -22,12 +26,17 @@ export class MemberListComponent implements OnInit {
   }
 
   loadMembers() {
-    this.membersService.getMembers(this.pageNumber, this.pageSize);
+    this.membersService.getMembers(this.userParams);
+  }
+
+  resetFilters() {
+    this.userParams = new UserParams(this.accountService.currentUser());
+    this.loadMembers();
   }
 
   pageChanged(event: any) {
-    if (this.pageNumber !== event.page) {
-      this.pageNumber = event.page;
+    if (this.userParams.pageNumber !== event.page) {
+      this.userParams.pageNumber = event.page;
       this.loadMembers();
     }
   }
