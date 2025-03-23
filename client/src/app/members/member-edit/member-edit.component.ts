2b4a8bce85757ef43cbb3e586c88085ddf8210a6
Charlie <charlychaky567@hotmail.com>
2025-03-23 10:35:22 -0600

    Parcial05 - Add TimeAgo module for date formatting

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,36 +1,55 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { MembersService } from '../../_services/members.service';
-import { MemberCardComponent } from '../member-card/member-card.component';
-import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { Component, HostListener, inject, OnInit, ViewChild } from '@angular/core';
+import { Member } from '../../_models/member';
 import { AccountService } from '../../_services/account.service';
-import { UserParams } from '../../_models/userParams';
+import { MembersService } from '../../_services/members.service';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { PhotoEditorComponent } from "../photo-editor/photo-editor.component";
+import { TimeagoModule } from 'ngx-timeago';
+import { DatePipe } from '@angular/common';
 
 @Component({
-  selector: 'app-member-list',
+  selector: 'app-member-edit',
   standalone: true,
-  imports: [MemberCardComponent, PaginationModule],
-  templateUrl: './member-list.component.html',
-  styleUrl: './member-list.component.css'
+  imports: [TabsModule, FormsModule, PhotoEditorComponent, TimeagoModule, DatePipe],
+  templateUrl: './member-edit.component.html',
+  styleUrl: './member-edit.component.css'
 })
-export class MemberListComponent implements OnInit {
-  private accountService = inject(AccountService);
-  membersService = inject(MembersService);
-  userParams = new UserParams(this.accountService.currentUser());
-  
+export class MemberEditComponent implements OnInit {
+  @ViewChild("editForm") editForm?: NgForm;
+  @HostListener("window:beforeunload", ["event"]) notify($event: any) {
+    if (this.editForm?.dirty) {
+      $event.returnValue = true;
+    }
+  }
+  member?: Member;
+  public accountService = inject(AccountService);
+  private membersService = inject(MembersService);
+  private toastr = inject(ToastrService);
+
   ngOnInit(): void {
-    if (!this.membersService.paginatedResult()) {
-      this.loadMembers();
-    }
+    this.loadMember();
   }
 
-  loadMembers() {
-    this.membersService.getMembers(this.userParams);
+  loadMember() {
+    const user = this.accountService.currentUser();
+    if (!user) return;
+    this.membersService.getMember(user.username).subscribe({
+      next: member => this.member = member
+    })
   }
 
-  pageChanged(event: any) {
-    if (this.userParams.pageNumber !== event.page) {
-      this.userParams.pageNumber = event.page;
-      this.loadMembers();
-    }
+  updateMember() {
+    this.membersService.updateMember(this.editForm?.value).subscribe({
+      next: _ => {
+        this.toastr.success("Profile updated!");
+        this.editForm?.reset(this.member);
+      }
+    });
+  }
+
+  onMemberChange(event: Member) {
+    this.member = event;
   }
 }
